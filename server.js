66e87067a902f29e5dc3a6877de1dd5399909d0d
SompaArya <sompalarya99@gmail.com>
2016-09-25 13:41:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.get('/article-one', function (req, res) {
   res.send('hi first page');
   
 });
-app.get('/article-two', function (req, res) {
-  res.send('hi first page');
-});
-  
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
