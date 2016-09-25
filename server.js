3e45675cf141d1f40f023426a8caff6b61a8c255
SompaArya <sompalarya99@gmail.com>
2016-09-25 14:57:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/a', function (req, res) {
   res.send('monu');
 });
 
